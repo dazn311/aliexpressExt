8ebfab40e0213f22ee428d08b2c5e44ca5915408
dazn311 <alex2505@bk.ru>
2024-09-23 03:49:59 +0300

    fix forwardRef

--- src/components/CardSpinner.jsx
+++ src/components/CardSpinner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const CardSpinner = React.forwardRef((ref) => (
+const CardSpinner = React.forwardRef((props,ref) => (
   <div ref={ref} className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
       <Link href={`#`}>
         <Image
