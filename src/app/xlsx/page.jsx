5d7a22c28125f4eaf2aefba4af87fb8384461db2
dazn311 <alex2505@bk.ru>
2024-10-11 05:34:57 +0300

    upd2 allLinesFromXls;
    [
      allLinesFromXls
      UpLoadXlsx
    ]

--- src/app/xlsx/page.jsx
+++ src/app/xlsx/page.jsx
@@ -1,77 +1,23 @@
 'use client';
 
 import React,{useState,useRef} from 'react';
-import {message} from 'antd';
-// import _get from 'lodash/get';
 import XLSX from 'xlsx-js-style';
 import UpLoadXlsx from "@/components/UpLoadXlsx";
-import {fileReader} from "@/utils/fileReader";
 import {getOS} from "@/utils/getOS";
 
 
 export default function XlsxPage({}) {
     const [fileD, setFileD] = useState({});
     const upLoadToWebRef = useRef(()=>{});
-    //TODO: test Windows or Mac OS;
     const checkValue = useRef({utf8:getOS() !== 'Windows'});
 
-    const saveFile = ({ file, onSuccess }) => {
-        // STEP 1: Create a new workbook
-        const wb = XLSX.utils.book_new();
-
-        // STEP 2: Create data rows and styles
-        let row = [
-            { v: "Courier: 24", t: "s", s: { font: { name: "Courier", sz: 24 } } },
-            { v: "bold & color", t: "s", s: { font: { bold: true, color: { rgb: "FF0000" } } } },
-            { v: "fill: color", t: "s", s: { fill: { fgColor: { rgb: "E9E9E9" } } } },
-            { v: "line\nbreak", t: "s", s: { alignment: { wrapText: true } } },
-        ];
-
-        // STEP 3: Create worksheet with rows; Add worksheet to workbook
-        const ws = XLSX.utils.aoa_to_sheet([row]);
-        XLSX.utils.book_append_sheet(wb, ws, "readme demo");
-
-        // STEP 4: Write Excel file to browser
-        XLSX.writeFile(wb, "xlsx-js-style-demo.xlsx");
-    }
-
-
     // 1.Step: read from user's file for upload to form;step.1
-    upLoadToWebRef.current = (info) => {
-        const {status} = info.file;//removed
-        console.log('46 status:',status)
-
-        switch (status) {
-            case 'done':
-                message.success(`${info.file.name} file uploaded successfully.`);
-                const uid = info.file.uid;
-                const size = info.file.size/1000;
-                const dataLines = size > 1000 ? `${(size/1000).toFixed(1)}Мб`: `${(size).toFixed(1)}Кб`;
-
-                setFileD(prev=>({...prev,
-                    [uid]:{fileData: info.file, dataLines: dataLines, fileName: info.file.name}
-                }));
-                return true;
-            case 'error':
-                message.error(`${info.file.name} file upload failed.`);
-                return true;
-            case 'removed':
-                message.info(`${info.file.name} file removed successfully.`);
-                const uid2 = info.file.uid;
-                setFileD(prev=>({...prev,
-                    [uid2]:null
-                }));
-                return true;
-            default:
-                if (status !== 'uploading') {
-                    // console.log(info.file, info.fileList);
-                    console.log('49 fileList: ', info.fileList);
-                }
-                return true;
-        }
+    upLoadToWebRef.current = (fileData) => {
+        console.log('46 fileData:',fileData);
+        setFileD(prev=>({...prev, ...fileData}));
     }
-    console.log('62 fileD:',fileD);
-    console.log('46 getOS():',getOS());
+
+    // console.log('62 fileD:',fileD);
     return (
         <div className='max-w-6xl mx-auto p-3 space-y-4'>
             <h1 className='text-2xl font-medium text-amber-600 text-center'>Excel Page</h1>
@@ -79,15 +25,12 @@ export default function XlsxPage({}) {
         </div>
     );
 }
-export const initFileData = {
-    0:{fileData: null, dataLines: '', fileName: ''}
-};
 
 const props = {
     name: 'file',
     multiple: true,
     // actions: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-    onChange2(info) {
+    // onChange(info) {
         // const {status} = info.file;//removed
         // console.log('83 status:',status)
         // if (status !== 'uploading') {
@@ -99,8 +42,28 @@ const props = {
         // } else if (status === 'error') {
         //     message.error(`${info.file.name} file upload failed.`);
         // }
-    },
+    // },
     onDrop(e) {
         console.log('Dropped files', e.dataTransfer.files);
     },
 };
+
+const saveFile = ({ file, onSuccess }) => {
+    // STEP 1: Create a new workbook
+    const wb = XLSX.utils.book_new();
+
+    // STEP 2: Create data rows and styles
+    let row = [
+        { v: "Courier: 24", t: "s", s: { font: { name: "Courier", sz: 24 } } },
+        { v: "bold & color", t: "s", s: { font: { bold: true, color: { rgb: "FF0000" } } } },
+        { v: "fill: color", t: "s", s: { fill: { fgColor: { rgb: "E9E9E9" } } } },
+        { v: "line\nbreak", t: "s", s: { alignment: { wrapText: true } } },
+    ];
+
+    // STEP 3: Create worksheet with rows; Add worksheet to workbook
+    const ws = XLSX.utils.aoa_to_sheet([row]);
+    XLSX.utils.book_append_sheet(wb, ws, "readme demo");
+
+    // STEP 4: Write Excel file to browser
+    XLSX.writeFile(wb, "xlsx-js-style-demo.xlsx");
+}
--- src/components/UpLoadXlsx.jsx
+++ src/components/UpLoadXlsx.jsx
@@ -1,4 +1,4 @@
-import {Upload} from 'antd';
+import {message, Upload} from 'antd';
 import {fileReader} from "@/utils/fileReader";
 
 const { Dragger } = Upload;
@@ -6,29 +6,54 @@ const { Dragger } = Upload;
 export default function UpLoadXlsx({data,checkValue,upLoadToWebRef}) {
 
     const onChangeHandler = ({ file,fileName='fileName-L8.xlsx', onSuccess }) => {
-        console.log('[9 onChangeHandler] file:',file)
+        // console.log('[9 onChangeHandler] file:',file)
+        const uid = file.uid;
         fileReader ({
             file: file,
             checkValue,
             fileName: fileName,
         })
             .then(data => {
+                upLoadToWebRef.current({
+                    [uid]:{fileData: data, dataLines: null, fileName: null}
+                });
                 onSuccess("ok");
-                // console.log('[15 onChangeHandler] data:',data)
             })
             .catch(e => {
-                onSuccess("ok");
-                console.log('60 failed:',e)
-                // message.error(`fileReader failed: ${e.message}`);
+                onSuccess("error");
+                upLoadToWebRef.current({
+                    [uid]:null
+                });
+                console.log('60 failed:',e);
             });
-        // setTimeout(() => {
-        //     onSuccess("ok");
-        // }, 0);
+    }
+    const onChangeHand = (info) => {
+        const {status} = info.file;//removed
+
+        switch (status) {
+            case 'done':
+                message.success(`${info.file.name} file uploaded successfully.`);
+                return true;
+            case 'error':
+                message.error(`${info.file.name} file upload failed.`);
+                return true;
+            case 'removed':
+                message.info(`${info.file.name} file removed successfully.`);
+                upLoadToWebRef.current({
+                    [info.file.uid]:null
+                });
+                return true;
+            default:
+                if (status !== 'uploading') {
+                    console.log('49 fileList: ', info.fileList);
+                }
+                return true;
+        }
     }
 
     return (
         <Dragger
-            onChange={upLoadToWebRef.current}
+            onChange={onChangeHand}
             customRequest={onChangeHandler}
             accept={'.xls,.xlsx,.csv,.txt'}
             {...data}>
